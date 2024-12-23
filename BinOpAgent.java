75df336c1e36efe8ba9025485fb9d3f1e675478d
Shahar Sulima <shahar0601@cs.colman.ac.il>
2024-12-24 01:19:43 +0200

    PTM1-EX1-Message,Topic ,TopicManagersingle from ex1,starting BinOpAgent

--- BinOpAgent.java
+++ BinOpAgent.java
@@ -4,7 +4,16 @@ import java.util.function.BinaryOperator;
 
 import test.TopicManagerSingleton.TopicManager;
 
-public class BinOpAgent{
+public class BinOpAgent implements Agent {
+    private String agentName;
+    private Topic first;
+    private Topic second;
+    private Topic end;
+    public BinOpAgent(String agentName, String atopic, String btopic,String ctopic,) {}
+    @Override
+    public void reset(){
+
+    }
 
     
 }
