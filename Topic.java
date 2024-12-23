9324ce4724989467995e4f5108494de3f50d6b69
ShaharSolema <shahar0601@cs.colman.ac.il>
2024-12-23 17:31:50 +0200

    PTM-ex3

--- Topic.java
+++ Topic.java
@@ -1,39 +1,26 @@
-package test;
-//package project_biu.graph;
-
+package project_biu.graph;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Topic {
     public final String name;
-    public List<Agent> subs;
-    public List<Agent> pubs;
     Topic(String name){
-        this.name=new String(name);
-        subs=new ArrayList<Agent>();
-        pubs=new ArrayList<Agent>();
+        this.name=name;
     }
 
     public void subscribe(Agent a){
-        subs.add(a);
     }
     public void unsubscribe(Agent a){
-        subs.remove(a);
     }
 
     public void publish(Message m){
-        for(Agent a : subs){
-            a.callback(this.name,m);
-        }
     }
 
     public void addPublisher(Agent a){
-        pubs.add(a);
     }
 
     public void removePublisher(Agent a){
-        pubs.remove(a);
     }
 
 
