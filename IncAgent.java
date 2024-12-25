3dde83c79b121b427267ad2a62d76582928da50e
Shahar Sulima <shahar0601@cs.colman.ac.il>
2024-12-25 18:22:42 +0200

    PTM1-EX4- IncAgent MathExampleConfig adding close method PlusAgent

--- IncAgent.java
+++ IncAgent.java
@@ -2,7 +2,8 @@ package test;
 
 import test.TopicManagerSingleton.TopicManager;
 
-public class IncAgent {
-
-    
+public class IncAgent extends PlusAgent {
+    public IncAgent(String[]subs,String[]pubs) {
+        super(new String[]{subs[0],subs[0]},pubs);
+    }
 }
--- PlusAgent.java
+++ PlusAgent.java
@@ -2,7 +2,11 @@ package test;
 
 import test.TopicManagerSingleton.TopicManager;
 
-public class PlusAgent {
+public class PlusAgent extends BinOpAgent {
+    private Number x=0.0;
+    private Number y=0.0;
+    public PlusAgent(String[]subs,String[] pubs) {
+        super("PlusAgent",subs[0],subs[1],pubs[0],(x,y)->x+y);
+    }
 
-    
 }
